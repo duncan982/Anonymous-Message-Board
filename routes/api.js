667a5292967ca691f21bef487e68e023afd22eda
lendoo <lendoo73@gmail.com>
2019-10-19 08:27:30 +0100

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -9,11 +9,22 @@
 'use strict';
 
 var expect = require('chai').expect;
+const threadHanler = require("../controllers/threadHandler.js");
+const replyHandler = require("../controllers/replyHandler.js");
 
 module.exports = function (app) {
   
-  app.route('/api/threads/:board');
+  app.route("/api/threads/:board")
+    .post(threadHanler.createThread)
+    .get(threadHanler.listThread)
+    .put(threadHanler.reportThread)
+    .delete(threadHanler.deleteThread)
+  ;
     
-  app.route('/api/replies/:board');
-
+  app.route("/api/replies/:board")
+    .post(replyHandler.createReply)
+    .put(replyHandler.reportReply)
+    .delete(replyHandler.deleteReply)
+    .get(replyHandler.showAllReplies)
+  ;
 };
